34b582521313938329b6ac0883546a8723b44c42
Lakshay <ls199575@gmail.com>
2020-09-25 21:32:34 +0530

    update routes

--- routes/superhero.js
+++ routes/superhero.js
@@ -2,56 +2,57 @@ const express = require('express');
 const router = express.Router();
 const Superhero = require('../models/Superhero');
 const config = require('config');
-router.get('/',async (req,res)=>{
-    const superheroes =await Superhero.find();
-    res.json({superheroes});
 
-     
+//Getting all superheroes
+router.get('/', async(req, res) => {
+    const superheroes = await Superhero.find();
+    res.json({superheroes});
 })
 
-router.post('/',async (req,res)=>{
+//Posting a new superhero
+router.post('/', async(req, res) => {
     const {name, powers, description, image} = req.body
-    try{
-        const superhero = new Superhero({name,powers,description,image});
+    try {
+        const superhero = new Superhero({name, powers, description, image});
         await superhero.save();
         res.json({superhero});
-    }
-    catch(err){
+    } catch (err) {
         console.error(err);
     }
-    
+
 });
 
-router.get('/:id',async (req,res)=>{
-    try{
+//finding a superhero
+router.get('/:id', async(req, res) => {
+    try {
         const superhero = await Superhero.findById(req.params.id);
-        res.json(superhero);    
-    }catch(err){
+        res.json(superhero);
+    } catch (err) {
         console.error(err);
     }
-    
+
 })
 
-router.patch('/:id',async (req,res)=>{
-    try{
-        
+//updating a superhero
+router.patch('/:id', async(req, res) => {
+    try {
+
         const superhero = await Superhero.findByIdAndUpdate(req.params.id, req.body, {new: true});
         res.json(superhero);
-        
 
-
-    }catch(err){
+    } catch (err) {
         console.error(err);
     }
 })
 
-router.delete('/:id',async (req,res)=>{
-    try{
+//deleting a superhero
+router.delete('/:id', async(req, res) => {
+    try {
         const superhero = await Superhero.findById(req.params.id);
         await superhero.remove();
         const msg = 'Deleted successfully';
         res.json(msg);
-    }catch(err){
+    } catch (err) {
         console.error(err);
     }
 })

fd831f3c9f72a32038a0cc98b845408b8d8a4fd2
Lakshay <ls199575@gmail.com>
2020-09-25 19:14:07 +0530

    superhero model added

---
+++ models/Superhero.js
@@ -0,0 +1,19 @@
+
+const mongoose = require('mongoose');
+
+
+const superheroSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    powers: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    }
+});
+
+module.exports = Superhero = mongoose.model('superhero',superheroSchema);
